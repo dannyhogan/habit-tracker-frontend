c9cd273bd95e3eb59d68c7107463ca2232ed52ac
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-07 15:53:55 -0700

    Fix auth0Provider

--- src/Auth0Provider.js
+++ src/Auth0Provider.js
@@ -1,6 +1,6 @@
-/* eslint-disable react/prop-types */
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import createAuth0Client from '@auth0/auth0-spa-js';
+import { setToken } from './services/habitsApi';
 
 const DEFAULT_REDIRECT_CALLBACK = () => {
   window.history.replaceState({},
@@ -11,8 +11,20 @@ const DEFAULT_REDIRECT_CALLBACK = () => {
 export const Auth0Context = React.createContext();
 export const useAuth0 = () => useContext(Auth0Context);
 
+export const withSession = Comp => {
+  return function WithSessionHOC(props) {
+    const { isAuthenticated, loading, auth0Client } = useAuth0();
+    if(!isAuthenticated && !loading) auth0Client.loginWithRedirect();
+
+    if(!isAuthenticated && loading) return null;
+
+    return <Comp {...props} />;
+  };
+};
+
+// eslint-disable-next-line react/prop-types
 export default function Auth0Provider({ children, onRedirectCallback = DEFAULT_REDIRECT_CALLBACK, ...initOptions }) {
-  const [isAuthenticated, updatedIsAuthenticated] = useState(false);
+  const [isAuthenticated, updateIsAuthenticated] = useState(false);
   const [user, setUser] = useState();
   const [auth0Client, setAuth0Client] = useState();
   const [loading, updateLoading] = useState(true);
@@ -28,11 +40,14 @@ export default function Auth0Provider({ children, onRedirectCallback = DEFAULT_R
       }
 
       const isAuthenticated = await auth0.isAuthenticated();
-      updatedIsAuthenticated(isAuthenticated);
+      updateIsAuthenticated(isAuthenticated);
 
       if(isAuthenticated) {
         const user = await auth0.getUser();
         setUser(user);
+
+        const claims = await auth0.getIdTokenClaims();
+        setToken(claims.__raw);
       }
 
       updateLoading(false);
@@ -45,10 +60,10 @@ export default function Auth0Provider({ children, onRedirectCallback = DEFAULT_R
       value={{
         isAuthenticated,
         user,
-        loading,
-        auth0Client
+        auth0Client,
+        loading
       }}>
       {children}
-    </ Auth0Context.Provider>
+    </Auth0Context.Provider>
   );
 }
