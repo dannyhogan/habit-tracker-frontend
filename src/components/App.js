82cb7ef1a002814eb20529c57f933d1de0d356ed
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-07 15:55:59 -0700

    Render containers in app, add Router, and update snapshot test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import { withSession } from '../Auth0Provider';
+import CreateHabit from '../containers/CreateHabit';
+import DisplayHabits from '../containers/DisplayHabits';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={withSession(CreateHabit)} />
+          <Route exact path="/habits" component={withSession(DisplayHabits)} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
