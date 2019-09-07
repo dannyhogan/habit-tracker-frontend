b05281c03c866df59660ea8740a6a5248c046393
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-07 15:58:19 -0700

    Add habitsApi services

---
+++ src/services/habitsApi.js
@@ -0,0 +1,38 @@
+let token = null;
+export const setToken = newToken => {
+  token = newToken;
+};
+
+export const postHabit = (habit, description) => {
+  return fetch(`${process.env.API_URL}/api/v1/habits`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify({ habit, description })
+  })
+    .then(res => {
+      if(!res.ok) throw 'Could not create a habit';
+      return res.json();
+    });
+};
+
+export const getHabits = () => {
+  console.log('called get habits in services');
+  return fetch(`${process.env.API_URL}/api/v1/habits`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  })
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Unable to get dogs.';
+      return json;
+    })
+    .then(json => {
+      return json;
+    });
+};
