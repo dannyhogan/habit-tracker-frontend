c71da5a2ade68ac0f380f9819ff21b360054c876
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-07 15:54:28 -0700

    Change store.js

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose } from 'redux';
+import reducer from './reducers';
 import { promiseMiddleware } from 'promise-middleware-redux';
-import reducer from './reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   reducer,
-  composeEnhancers(
-    applyMiddleware(promiseMiddleware)
-  )
+  composeEnhancers(applyMiddleware(promiseMiddleware))
 );
