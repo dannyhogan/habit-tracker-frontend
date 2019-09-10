dddd860fb1648d15b7d7db3525494e503ead4783
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-10 09:59:05 -0700

    Create presentational component to combine containers

---
+++ src/containers/Habits.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CreateHabit from './CreateHabit';
+import DisplayHabits from './DisplayHabits';
+
+const Habits = () => {
+  return (
+    <>
+      <CreateHabit />
+      <DisplayHabits />
+    </>
+  );
+};
+
+export default Habits;
